3771b2d426ffff00d529f2ecd9b2538c47e11ebf
GAC <129469688+Gretly@users.noreply.github.com>
2023-06-08 22:10:23 -0500

    RESONDER

--- joseluis.cpp/3.1.cpp
+++ joseluis.cpp/3.1.cpp
@@ -11,6 +11,6 @@ h = sqrt( a * a + b * b);
 cout << " lado 1 = " << a << endl;
 cout << " lado 2 = " << b << endl;
 cout << " hipotenusa = " << h << endl;
-system("PAUSE");
-return EXIT_SUCCESS;
-}
+system("PAUSE"); // QUE INDICA ESTA LINEA DE CODIGO
+return EXIT_SUCCESS; // QUE INDICA ESTA LINEA DE CODIGO
+} // PARA EL CASO QUE LA HIPOTENUSA SEA MAYOR QUE 10, TRIPLIQUE SU VALOR; CASO CONTRARIO LE INCREMETE 2
